c7dedf20b8cd39e93d4c131aacb601c17d24f57f
Jirka Kremser <jkremser@redhat.com>
2019-11-15 23:58:40 +0100

    add configurable wait

--- src/main/java/example/micronaut/QuoteController.java
+++ src/main/java/example/micronaut/QuoteController.java
@@ -2,11 +2,19 @@ package example.micronaut;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 @Controller("/quotes")
 public class QuoteController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(QuoteController.class);
+
     private final QuoteService quoteService;
+
+    private final Long wait = Optional.ofNullable(System.getenv("WAIT")).map(Long::parseLong).orElse(0L);
 
     public QuoteController(QuoteService quoteService) {
         this.quoteService = quoteService;
@@ -14,6 +22,14 @@ public class QuoteController {
 
     @Get("/random")
     public Quote randomQuote() {
+        LOG.info("Got request - sleeping for " + wait + "ms");
+        if (wait > 0L) {
+            try {
+                Thread.sleep(wait);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
         return quoteService.randomQuote();
     }
