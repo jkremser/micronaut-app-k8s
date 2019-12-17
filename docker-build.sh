49c68324c3e661a76612746c55ce2d79400aa8f4
Jirka Kremser <jiri.kremser@oracle.com>
2019-12-17 21:21:43 +0100

    backup

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,7 @@
-docker build . -f Dockerfile.native -t micronaut-graal-app:native
-docker build . -f Dockerfile.jit -t micronaut-graal-app:jit
+ORG=${ORG:-"jkremser/"}
+
+docker build . -f Dockerfile.native -t ${ORG}micronaut-graal-app:native
+docker build . -f Dockerfile.jit -t ${ORG}micronaut-graal-app:jit
 echo
 echo
 echo "To run the docker container execute:"
